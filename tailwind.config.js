53c992c3c1ff6ce8b5beecb79f6fded1eba7a209
Erika <adob.e@hotmail.com>
2023-01-10 13:50:44 +0100

    feat: add style first part oh home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,27 @@ module.exports = {
     "./templates/**/*.html.twig"
   ],
   theme: {
+    fontFamily : {
+      'primary' : ["'Petit Formal Script', cursive"],
+      'secondary' : ["'Alice', serif"],
+      'third' : ["'Open Sans', sans-serif"]
+    },
+    fontSize : {
+      '2.25xl' : '1.625rem',
+      '2.5xl' : '1.75rem',
+      '3xl' : "1.875rem",
+      '3.5xl': '2rem',
+    },
     colors: {
       'primary' : '#171515',
       'secondary' : '#F9F6F0',
     },
-    extend: {},
+    extend: {
+      blur : {
+        sm : '2px',
+        md : '4px',
+      },
+    },
   },
   plugins: [],
 }
