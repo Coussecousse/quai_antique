de0588f26092804ccc19408b5165e722c53b9e84
Erika <adob.e@hotmail.com>
2023-01-12 08:40:12 +0100

    style: home desk

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,10 @@ module.exports = {
       'third' : ["'Open Sans', sans-serif"]
     },
     fontSize : {
-      'sm' : '.875rem',
+      'home-title' : 'clamp(2rem, 1.282rem + 3.59vw, 3.75rem)',
+      'home-quai' : 'clamp(2.25rem, 1.224rem + 5.128vw, 4.75rem)',
+      'home-carousel' : 'clamp(1rem, 0.857rem + 0.714vw, 1.5rem)',
+      'home-title-moving' : 'clamp(2rem, 1.795rem + 1.026vw, 2.5rem)',
       'lg' : "1.125rem",
       '2xl' : '1.5rem',
       '2.25xl' : '1.625rem',
@@ -26,6 +29,9 @@ module.exports = {
       '500' : '#6F5B3E',
     },
     extend: {
+      height : {
+        'carousel-image' : 'clamp(13.375rem, 7.107rem + 31.339vw, 35.313rem)',
+      },
       blur : {
         sm : '2px',
         md : '4px',
