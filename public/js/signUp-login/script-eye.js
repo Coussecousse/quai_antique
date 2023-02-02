285633ffc876fb68fe6f629e2a3089308bbc2ffc
Erika <adob.e@hotmail.com>
2023-02-02 10:25:07 +0100

    feat: see/unsee password

---
+++ public/js/signUp-login/script-eye.js
@@ -0,0 +1,33 @@
+function togglePassword(e) {
+    const target = e.target.parentElement;
+    
+    const eye = target.querySelector('.fa-eye');
+    const eyeSlash = target.querySelector('.fa-eye-slash');
+    const password = target.previousElementSibling;
+
+    console.log(eye, eyeSlash, password);
+    console.log(eye.classList.contains('hide'));
+
+    eye.classList.remove("lala");
+
+    // if (eye.classList.contains('hide')){
+    //     console.log('hey');
+    //     eye.classList.remove("hide");
+    // } else {
+    //     console.log('yo')
+    //     eye.classList.add("hide");
+    // }
+
+    if (!eye.classList.contains("hide")) {
+        eye.classList.add("hide");
+        eyeSlash.classList.remove("hide");
+    } else {
+        eye.classList.remove("hide");
+        console.log(eyeSlash);
+        eyeSlash.classList.add("hide");
+    }
+    // eye.classList.contains('hide') ? eye.classList.remove('hide') : eye.classList.add("hide");
+    // eyeSlash.classList.contains("hide") ? eyeSlash.classList.remove("hide") : eye.classList.add("hide");
+    password.type = password.type === "text" ? "password" : "text";
+
+}
