33cfe0217fe2c33dc7d2d185de64e93904867096
Erika <adob.e@hotmail.com>
2023-04-11 11:34:02 +0200

    feat: right way

--- public/js/home/script.js
+++ public/js/home/script.js
@@ -11,31 +11,40 @@ const screenWidth = window.innerWidth;
 
 const middleScreen = screenWidth/2;
 const soustraction = middleElement - middleScreen;
+
 const carousel = document.querySelector(".carousel_pics");
-carousel.style.transform = soustraction > 0 ? "translateX(calc(-" + (soustraction) + 'px))': "translateX(calc(" + (soustraction * -1)  + 'px))';
-for (let child of carousel.children) {
-    child.classList.add('duration-150');
-}
+carousel.style.transform = soustraction > 0 ? "translateX(-" + soustraction + 'px)': "translateX(" + (soustraction * -1)  + 'px)';
 
-function centerElement() {
-    const activePic = document.querySelector('div[data-active="true"]');
-
-    const rect = activePic.getBoundingClientRect();
-
-    const leftPosition = rect.left;
-
-    const widthElement = activePic.clientWidth;
-    const middleElement = leftPosition + widthElement/2;
-
-    const screenWidth = window.innerWidth;
-
-    const middleScreen = screenWidth/2;
-    const soustraction = middleElement - middleScreen;
-
-    const carousel = document.querySelector(".carousel_pics");
-    carousel.style.transform = soustraction > 0 ? "translateX(-" + (soustraction) + 'px)': "translateX(" + (soustraction * -1)  + 'px)';
-}
-
+// for (let child of carousel.children) {
+//     child.classList.add('duration-150');
+// }
+window.addEventListener("resize", () => {
+    setTimeout(() => {
+        const carousel = document.querySelector(".carousel_pics");
+    
+        const style = window.getComputedStyle(carousel);
+        const transform = new WebKitCSSMatrix(style.transform);
+    
+        const activePic = document.querySelector('div[data-active="true"]');
+    
+        const rect = activePic.getBoundingClientRect();
+    
+        const leftPosition = rect.left;
+    
+        const widthElement = activePic.clientWidth;
+        console.log(widthElement)
+    
+        const middleElement = leftPosition + widthElement/2;
+    
+        const screenWidth = window.innerWidth;
+    
+        const middleScreen = screenWidth/2;
+    
+        const soustraction = middleScreen - middleElement ;
+        carousel.style.transform = "translateX(" + (transform.e + soustraction) + 'px)';
+    }, 500);
+    
+});
 function centerNextElement() {
     const carousel = document.querySelector(".carousel_pics");
     
@@ -43,6 +52,7 @@ function centerNextElement() {
     const transform = new WebKitCSSMatrix(style.transform);
     
     const active = document.querySelector("div[data-active='true']");
+
     let next;
     if (active.dataset.image == carousel.children.length - 1) {
         next = document.querySelector("div[data-image='0']");
@@ -64,11 +74,11 @@ function centerNextElement() {
     const middleScreen = screenWidth/2;
     
     if (next.dataset.image == carousel.children.length - 1) {
-        const addition = middleElement - leftPosition - middleScreen + 16;
-        carousel.style.transform = addition > 0 ? "translateX(" + addition + "px)" : "translateX(" + ((addition * -1)) + "px)";
+        const soustraction = middleElement - leftPosition - middleScreen;
+        carousel.style.transform = soustraction > 0 ? "translateX(" + soustraction + "px)" : "translateX(" + ((soustraction * -1)) + "px)";
     } else {
-        const soustraction = middleElement - middleScreen;
-        carousel.style.transform = soustraction > 0 ? "translateX(" + (transform.e - soustraction) + 'px)': "translateX(" + (transform.e - (soustraction * -1))  + 'px)';
+        const soustraction = middleScreen - middleElement;
+        carousel.style.transform = "translateX(" + (transform.e + soustraction) + 'px)';
     }
     
     if (active.dataset.image == 0 ) {
@@ -89,39 +99,67 @@ function centerNextElement() {
 
 }
 
-window.addEventListener("resize", () => {
+function centerPreviousElement() {
     const carousel = document.querySelector(".carousel_pics");
-
+    
     const style = window.getComputedStyle(carousel);
     const transform = new WebKitCSSMatrix(style.transform);
+    
+    const active = document.querySelector("div[data-active='true']");
+    let previous;
+    if (active.dataset.image == 0) {
+        previous = carousel.children[carousel.children.length - 1];
+    } else {
+        previous = active.previousElementSibling;
+    }
 
-    const activePic = document.querySelector('div[data-active="true"]');
-
-    const rect = activePic.getBoundingClientRect();
-
+    active.dataset.active = false;
+    previous.dataset.active = true;
+    
+    const rect = previous.getBoundingClientRect();
     const leftPosition = rect.left;
-
-    const widthElement = activePic.clientWidth;
-
+    
+    const widthElement = previous.clientWidth;
     const middleElement = leftPosition + widthElement/2;
-
+    
     const screenWidth = window.innerWidth;
-
+    
     const middleScreen = screenWidth/2;
+    
+    if (previous.dataset.image == carousel.children.length - 1) {
+        const soustraction = middleElement - leftPosition - middleScreen;
+        carousel.style.transform = soustraction > 0 ? "translateX(" + soustraction + "px)" : "translateX(" + ( soustraction * -1) + "px)";
+    } else {
+        const soustraction = middleElement - middleScreen;
+        carousel.style.transform = soustraction > 0 ? "translateX(" + (transform.e - soustraction) + 'px)': "translateX(" + (transform.e + (soustraction * -1))  + 'px)';
+    }
 
-    const soustraction = middleScreen - middleElement ;
+    if (active.dataset.image == carousel.children.length - 1){
+        const nextToLastActive = document.querySelector("div[data-image='0']");
+        nextToLastActive.classList.add('invisible', 'opacity-0');
+    } else {
+        const nextToLastActive = active.nextElementSibling;
+        nextToLastActive.classList.add('invisible', 'opacity-0');
+    }
 
-    carousel.style.transform = soustraction > 0 ? "translateX(" + (transform.e + soustraction) + 'px)': "translateX(" + (transform.e - (soustraction * -1))  + 'px)';
-});
+    if (previous.dataset.image == 0 ) {
+        const previousToNewActive = carousel.children[carousel.children.length - 1];
+        previousToNewActive.classList.remove("invisible", "opacity-0");
+    } else {
+        const previousToNewActive = previous.previousElementSibling;
+        previousToNewActive.classList.remove("invisible", "opacity-0");
+    }
+
+}
 
 const buttons = document.querySelector("#home_buttons");
 
 buttons.addEventListener("click", e => {
 
     if (e.target.parentElement.ariaLabel == "gauche") {
-        centerNextElement();
+        centerPreviousElement();
     } else if (e.target.parentElement.ariaLabel == "droite") {
-        
+        centerNextElement();
     } else  {
 
     }
