6f2f8009335f9d75a03c16aeabbad2f238fa65fc
Erika <adob.e@hotmail.com>
2023-04-13 18:35:51 +0200

    bug: block button waiting for animation

--- public/js/global-script.js
+++ public/js/global-script.js
@@ -2,31 +2,29 @@ const menuButton = document.querySelector("#menu-hamburger");
 const header = document.querySelector("header");
 
 menuButton.addEventListener("click", () => {
-
     menuButton.disabled = true;
+    setTimeout(() => {
+        menuButton.disabled = false;
+    }, 1000);
+    const menu = document.querySelector("#header-menu");
+
     if (menuButton.children[0].classList.contains('close')) {
         menuButton.children[0].classList.replace('close', 'open');
         header.style.position = "fixed";
-    } else {
-        menuButton.children[0].classList.replace('open', 'close');
-        header.style.position = "";
-    }
-    
-    const menu = document.querySelector("#header-menu");
-        if (!menu.classList.contains('open')) {
         menu.classList.remove("hidden");
         setTimeout(() => {
             menu.classList.add("open");
         }, 100);
     } else {
+        menuButton.children[0].classList.replace('open', 'close');
+        header.style.position = "";
         menu.classList.remove("open");
         setTimeout(() => {
             menu.classList.add('hidden');
         }, 1000);
     }
-    setTimeout(() => {
-        menuButton.disabled = false;
-    }, 600);
+
+
 })
 
 // Sticky menu
