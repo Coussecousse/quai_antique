c160abbb2954391a061508bb8b470b6115c76488
Erika <adob.e@hotmail.com>
2023-04-14 11:49:02 +0200

    feat: new card style

--- public/js/card/script-card.js
+++ public/js/card/script-card.js
@@ -1,36 +1,36 @@
-aArray = document.querySelectorAll('.diamonds li a');
-content = document.querySelector(".main-content");
-button = document.querySelector("#card-booking");
+// aArray = document.querySelectorAll('.diamonds li a');
+// content = document.querySelector(".main-content");
+// button = document.querySelector("#card-booking");
 
-aArray.forEach(a => {
-    a.addEventListener("click", e => {
-        e.preventDefault();
-        for (let element of aArray) {
-            if (element !== a ){
-                switch (a.parentElement.id) {
-                    case "card-starter":
-                        element.parentElement.classList.add("bottom");
-                        break;
-                    case "card-main": 
-                        element.parentElement.classList.add("right");
-                        break;
-                    case "card-dessert": 
-                        element.parentElement.classList.add("left");
-                        break;
-                    case "card-menus": 
-                        element.parentElement.classList.add('top');
-                        break;
-                }
-            } else {
-                element.parentElement.classList.add('active');
-            }
-            button.style.opacity = "0";
-            setTimeout(() => {
-                a.parentElement.classList.add("moving");
-            }, 300);
-            setTimeout(() => {
-                window.location.href = a.href;
-            }, 800);
-        }
-    })
-});
+// aArray.forEach(a => {
+//     a.addEventListener("click", e => {
+//         e.preventDefault();
+//         for (let element of aArray) {
+//             if (element !== a ){
+//                 switch (a.parentElement.id) {
+//                     case "card-starter":
+//                         element.parentElement.classList.add("bottom");
+//                         break;
+//                     case "card-main": 
+//                         element.parentElement.classList.add("right");
+//                         break;
+//                     case "card-dessert": 
+//                         element.parentElement.classList.add("left");
+//                         break;
+//                     case "card-menus": 
+//                         element.parentElement.classList.add('top');
+//                         break;
+//                 }
+//             } else {
+//                 element.parentElement.classList.add('active');
+//             }
+//             button.style.opacity = "0";
+//             setTimeout(() => {
+//                 a.parentElement.classList.add("moving");
+//             }, 300);
+//             setTimeout(() => {
+//                 window.location.href = a.href;
+//             }, 800);
+//         }
+//     })
+// });
