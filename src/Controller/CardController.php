773d628fe233a0b2e9db2c7be1054793e94273c5
Erika <adob.e@hotmail.com>
2023-04-16 12:11:48 +0200

    feat: change card design

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\FoodRepository;
+use App\Repository\MenuRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -12,19 +14,39 @@ class CardController extends AbstractController
         return $this->render('Card/card.html.twig');
     }
     #[Route('/carte/entrées', name:'card-starter')]
-    public function cardStarter() {
-        return $this->render('Card/Starter/starter.card.html.twig');
+    public function cardStarter(FoodRepository $repository) {
+
+        $foods = $repository->findBy(['category' => 'starter']);
+
+        return $this->render('Card/Starter/starter.card.html.twig', [
+            'foods' => $foods,
+        ]);
     }
     #[Route('/carte/plats', name:'card-main')]
-    public function cardMain() {
-        return $this->render('Card/Main/main.card.html.twig');
+    public function cardMain(FoodRepository $repository) {
+
+        $foods = $repository->findBy(['category' => 'main']);
+
+        return $this->render('Card/Main/main.card.html.twig', [
+            'foods'=> $foods
+        ]);
     }
     #[Route('/carte/desserts', name:'card-dessert')]
-    public function cardDessert() {
-        return $this->render('Card/Dessert/dessert.card.html.twig');
+    public function cardDessert(FoodRepository $repository) {
+
+        $foods = $repository->findBy(['category' => 'dessert']);
+
+        return $this->render('Card/Dessert/dessert.card.html.twig', [
+            'foods'=> $foods
+        ]);
     }
     #[Route('/carte/menus', name:'card-menus')]
-    public function cardMenus() {
-        return $this->render('Card/Menus/menus.card.html.twig');
+    public function cardMenus(MenuRepository $repository) {
+
+        $menus = $repository->findAll();;
+
+        return $this->render('Card/Menus/menus.card.html.twig', [
+            'menus' => $menus
+        ]);
     }
 }
