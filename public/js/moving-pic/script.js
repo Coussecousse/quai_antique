ff5fc7a5f951e8305dec2cca35016b4cf7a06305
Erika <adob.e@hotmail.com>
2023-04-20 09:45:11 +0200

    feat: change image

---
+++ public/js/moving-pic/script.js
@@ -0,0 +1,59 @@
+const container = document.querySelector("#moving-pic");
+
+const imagesClass = ['image_1', 'image_2', 'image_3','image_4', 'image_5'];
+
+
+// const interval = setInterval(() => {
+//     let page = location.href;
+//     page = currentRoute.split("/").pop();
+//     console.log(page)
+//     switch (page) {
+//         case 'reservation':
+            
+//             break;
+//     }
+// }, 1000);
+let page = location.href;
+page = page.split("/").pop();
+console.log(page)
+
+switch(page) {
+    case 'reservation':
+        changeImage('image_1', 0);
+        break;
+    case 'contact':
+        changeImage('image_5', imagesClass.length - 1);
+        break;
+    case 'connexion': 
+        changeImage('image_3',2);
+        break;
+    case 'inscription':
+        changeImage('image_2', 1);
+        break;
+}
+
+function changeImage(className, index) {
+    container.classList.add(className);
+    let currentClass = className;
+
+    setTimeout(() => {
+        setInterval(() => {
+
+            container.classList.contains('image-coming') ? 
+            container.classList.replace('image-coming', 'image-leaving') : 
+            container.classList.add('image-leaving');
+
+            setTimeout(() => {
+                container.classList.remove(currentClass);
+                if (index == imagesClass.length - 1) {
+                    currentClass = imagesClass[0];
+                    index = 0;
+                } else {
+                    currentClass = imagesClass[index += 1];
+                }
+                container.classList.add(currentClass);
+                container.classList.replace('image-leaving', 'image-coming')
+            }, 1000);
+        }, 6000)
+    }, 0);
+}
